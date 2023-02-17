573af0b9084f9651240fa0dbed953b0c9941761d
Afaz Khatri <khatriafaz@gmail.com>
2023-02-17 17:23:03 +0530

    Updated the dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/inertia-react';
+import { Card, Typography } from 'antd';
 
 export default function Dashboard(props) {
     return (
@@ -10,13 +11,9 @@ export default function Dashboard(props) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+            <Card title={`Welcome, ${props.auth.user.name}`}>
+                <Typography.Text>You're logged in!</Typography.Text>
+            </Card>
         </AuthenticatedLayout>
     );
 }
